d2ba10a0671023d0af1bbd3d8ade347d12e29411
Michał Wróbel <wrobel604@hotmail.com>
2021-04-06 14:16:52 +0200

    Create database model.

--- easywarehouse/products/models.py
+++ easywarehouse/products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Categories(models.Model):
+    name = models.CharField(max_length=50)
+
+class Products(models.Model):
+    category = models.ForeignKey(Categories, models.DO_NOTHING)
+    universal_id = models.UUIDField(unique=True)
+    name = models.CharField(max_length=50)
+    quantity = models.IntegerField()
+    tags = models.TextField(blank=True, null=True)
+
+class Images(models.Model):
+    url = models.CharField(max_length=255)
+    product = models.ForeignKey('Products', models.DO_NOTHING)
