1511b8f3e06c08f83b36e0fa510781c828a29cea
Michał Wróbel <wrobel604@hotmail.com>
2021-04-07 15:00:33 +0200

    Changes in models

--- easywarehouse/products/models.py
+++ easywarehouse/products/models.py
@@ -1,16 +1,27 @@
 from django.db import models
 
-# Create your models here.
-class Categories(models.Model):
+
+class Category(models.Model):
     name = models.CharField(max_length=50)
 
-class Products(models.Model):
-    category = models.ForeignKey(Categories, models.DO_NOTHING)
-    universal_id = models.UUIDField(unique=True)
+    class Meta:
+        db_table = "categories"
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Categories, on_delete=models.DO_NOTHING)
+    public_id = models.UUIDField(unique=True)
     name = models.CharField(max_length=50)
-    quantity = models.IntegerField()
+    quantity = models.PositiveIntegerField()
     tags = models.TextField(blank=True, null=True)
 
-class Images(models.Model):
+    class Meta:
+        db_table = "products"
+
+
+class Image(models.Model):
     url = models.CharField(max_length=255)
-    product = models.ForeignKey('Products', models.DO_NOTHING)
+    product = models.ForeignKey("Products", on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "images"
