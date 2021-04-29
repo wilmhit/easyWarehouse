6f33f70223b5e738f1ddb75ce4c2e38e422df8d9
Michał Wróbel <wrobel.m.michal@gmail.com>
2021-04-29 18:14:52 +0200

    Add index.html to views. Add serchbox in index.html. Change login.html to template format.

--- easywarehouse/products/views.py
+++ easywarehouse/products/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('<h1>Hello, this is index page</h1> <a href="/login">login</a>')
+    # return HttpResponse('<h1>Hello, this is index page</h1> <a href="/login">login</a>')
+    return render(request, "../templates/index.html", content_type="text/html")
 
 
 @login_required
