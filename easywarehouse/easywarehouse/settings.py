8ae67114c03a8465df79728d0a0cb6cc62dbc47a
Jcob Chodubski <jchodubski@pm.me>
2021-05-18 19:38:26 +0200

    Remove unnecessary settings

--- easywarehouse/easywarehouse/settings.py
+++ easywarehouse/easywarehouse/settings.py
@@ -121,8 +121,6 @@ USE_TZ = True
 
 # Media Files
 
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
 AWS_DEFAULT_ACL = "public-read"
 AWS_S3_CUSTOM_DOMAIN = f"s3.eu-central-1.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}"
