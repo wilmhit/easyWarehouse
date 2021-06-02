a69d30905b2b60910b8d06498d9355b71bb229f6
songofroland <46332508+songofroland@users.noreply.github.com>
2021-06-02 11:58:18 +0200

    Implement showing product availability (#44)
    * Implement showing product availability
    
    * CR fixes
    
    * Resolve conflicts
    
    * Fix migrations
    
    * Fix product search
    
    * Fix details page
    
    * Migrations
    
    Co-authored-by: Jcob Chodubski <jchodubski@pm.me>

--- easywarehouse/products/views.py
+++ easywarehouse/products/views.py
@@ -1,5 +1,6 @@
+import json
+
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import DetailView, ListView
@@ -16,41 +17,57 @@ def index(request):
     return render(request, "index.html")
 
 
-class ProductSearch(ListView):
+class ProductAvailabilityMixin:
+    @staticmethod
+    def get_ratio_json(product: Product) -> str:
+        return json.dumps(
+            {
+                "id": str(product.public_id),
+                "ratio": product.quantity / product.good_availability_threshold,
+            }
+        )
+
+
+class GuestListProducts(ListView, ProductAvailabilityMixin):
     template_name = "product_search.html"
     models = Product
-    queryset = Product.objects.all()
 
-    def get_queryset(self):
-        query = self.request.GET.get("text-search", "")
+    def get_queryset(self, default_text_query: str = ""):
+        query = self.request.GET.get("text-search", default_text_query)
         matched_products = ProductDocument.search().query(
             "simple_query_string", query=query, fields=["name^5", "description"]
         )
-        return matched_products
+        return matched_products.to_queryset()
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        products_with_ratios = [self.get_ratio_json(p) for p in context["object_list"]]
+        context["products_with_ratios"] = products_with_ratios
+        return context
 
 
-class ProductDetails(DetailView):
+class GuestProductDetails(DetailView, ProductAvailabilityMixin):
     template_name = "product_details.html"
     model = Product
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["product_with_ratio"] = self.get_ratio_json(context["object"])
+        return context
 
 
 # Employee views
 
 
-class ListProducts(LoginRequiredMixin, ProductSearch):
+class ProductDetails(LoginRequiredMixin, GuestProductDetails):
+    template_name = "employee/products/details.html"
+
+
+class ListProducts(GuestListProducts):
     template_name = "employee/products/list.html"
 
-    def get_queryset(self):
-        query = self.request.GET.get("text-search", "*")
-        matched_products = ProductDocument.search().query(
-            "simple_query_string", query=query, fields=["name^5", "description"]
-        )
-        return matched_products
-
-
-class EmployeeProductDetails(LoginRequiredMixin, DetailView):
-    template_name = "employee/products/details.html"
-    model = Product
+    def get_queryset(self, default_text_query: str = "*"):
+        return super().get_queryset(default_text_query)
 
 
 class AddProduct(LoginRequiredMixin, CreateView):
