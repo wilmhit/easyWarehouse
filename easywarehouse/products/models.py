b1b300b8fc7f45de9c03a70639c9ce5c90a90f2f
Roland Jaloszynski <rolandj@protonmail.com>
2021-06-08 21:30:59 +0200

    Fix elasticsearch synchronisations

--- easywarehouse/products/models.py
+++ easywarehouse/products/models.py
@@ -4,7 +4,12 @@ from categories.models import Category
 from django.contrib.postgres.fields import ArrayField
 from django.core.validators import RegexValidator
 from django.db import models
+from django.db.models.base import ModelBase
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
 from django.urls import reverse
+from easywarehouse.settings import ELASTICSEARCH_HOST, ELASTICSEARCH_PORT, PRODUCTS_INDEX_NAME
+from elasticsearch import Elasticsearch
 
 
 class Product(models.Model):
@@ -25,6 +30,9 @@ class Product(models.Model):
     )
 
     def __str__(self):
+        return self.name
+
+    def __repr__(self):
         return f"Product(name={self.name})"
 
     def get_absolute_url(self):
@@ -32,3 +40,11 @@ class Product(models.Model):
 
     class Meta:
         db_table = "products"
+
+
+@receiver(pre_delete, sender=Product, dispatch_uid="question_delete_signal")
+def log_deleted_question(sender: ModelBase, instance: Product, using: str, **kwargs):
+    Elasticsearch(hosts=[{"host": ELASTICSEARCH_HOST, "port": ELASTICSEARCH_PORT}]).delete(
+        PRODUCTS_INDEX_NAME, instance.id
+    )
+    instance.save()
